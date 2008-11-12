cc6fe2f136c639523f2a4e44c1f5c0635847491a
Yuffster <msteigerwalt@gmail.com>
2008-11-12 03:15:16 +0000

    Moving files around.

--- core/views/error.php
+++
@@ -1,8 +0,0 @@
-<h2>We're Sorry!</h2>
-
-<p>There was an error.</p>
-
-<pre>
-<? /* You'll probably want to disable full error output before releasing your app. */ ?>
-<?print_r($result);?>
-</pre>
--- core/views/index.php
+++
@@ -1,5 +0,0 @@
-<p>Welcome to RabidCore!  It's installed properly.</p>
-
-<p>The template file resides in views/template.php, and this text is in views/index.php.</p>
-
-<p>You'll probably want to modify these files to do something more useful.</p>
